a92e4ab3d1c31800196139682d0471848cd3bbb2
Becky0546 <becky@tumblbug.com>
2023-03-28 18:57:48 +0900

    chatGPT UI update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { BeatLoader } from "react-spinners";
+import { v4 as uuidv4 } from "uuid";
+import { TfiClose } from "react-icons/tfi";
+import { IoIosArrowForward } from "react-icons/io";
 import ChatWindow from "./components/ChatWindow";
 
 interface messageType {
@@ -13,6 +16,7 @@ function App() {
   const [message, setMessage] = useState<messageType[]>([]);
   const [loading, setLoading] = useState(true);
   const [inputValue, setInputValue] = useState("");
+  const messageContainerRef = useRef<HTMLDivElement>(null);
 
   const sendRequest = async (userInput: string) => {
     const question: messageType = {
@@ -41,7 +45,7 @@ function App() {
   };
 
   const sendQuestion = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13 && inputValue !== "") {
       const inputElement = e.target as HTMLInputElement;
       setInputValue("");
       sendRequest(inputElement.value);
@@ -55,25 +59,54 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useLayoutEffect(() => {
+    const messageContainer = messageContainerRef.current;
+    if (messageContainer) {
+      messageContainer.scrollTop = messageContainer.scrollHeight;
+    }
+  }, [message]);
+
   return (
-    <div className="flex flex-col items-center h-screen bg-gray-100 font-sans">
-      <div className="block shadow-md text-left flex items-center justify-left p-5 w-[380px] h-[52px] bg-[#F1F0FA] rounded-t-2xl absolute mt-10 z-50 m-5 font-sofia-sans font-bold text-18 leading-22 tracking-tighter text-[#C1BBE5]">
-        STEADIO GPT
+    <div className="flex flex-col items-center h-screen bg-gray-100 font-gothic font-normal leading-4">
+      <div
+        className="text-left flex items-center justify-left p-5 w-[372px] h-[52px] bg-[#F1F0FA] rounded-t-2xl 
+                  absolute mt-10 z-50 m-5 font-sofia-sans font-bold text-18 leading-22 tracking-tighter text-[#C1BBE5]"
+      >
+        <div className="rounded-2xl bg-[#48b51c] w-2 h-2 mr-2" />
+        <div>STEADIO GPT</div>
+        <button className="ml-[200px]">
+          <TfiClose color="#7D69FE" />
+        </button>
       </div>
-      <div className="pt-[70px] w-[380px] h-[630px] mt-10 bg-white shadow-2xl drop-shadow-2xl rounded-2xl overflow-y-scroll scrollbar-hide relative z-0">
+      <div
+        ref={messageContainerRef}
+        className="tracking-tight pt-[64px] pb-[64px] w-[372px] h-[630px] mt-10 bg-white shadow-2xl drop-shadow-2xl 
+                  rounded-2xl overflow-y-scroll overflow-x-hidden scrollbar-hide relative z-0"
+      >
         <ul className="w-96">
           {message.length !== 0 &&
-            message.map((item, idx) => (
-              <ChatWindow messages={item} key={idx} />
+            message.map((item) => (
+              <ChatWindow messages={item} key={uuidv4()} />
             ))}
-          {loading && <BeatLoader color="#5D49D5" size={10} className="mt-5" />}
+          {loading && (
+            <div className="w-full h-full flex justify-center items-center">
+              <BeatLoader color="#5D49D5" size={10} className="mt-6 mb-6" />
+            </div>
+          )}
         </ul>
       </div>
-      <div>
+      <div className="absolute mt-[610px]">
+        {inputValue === "" && (
+          <IoIosArrowForward
+            color="#C1BBE5"
+            size={24}
+            className="absolute mt-3 ml-3"
+          />
+        )}
         <input
           type={"text"}
           value={inputValue}
-          className="bg-[#F1F0FA] rounded-2xl p-5 z-50 w-[360px] h-[50px] absolute top-[610px] left-[540px] border-[1px] border-[#E2DFE3]"
+          className="bg-[#F1F0FA] rounded-2xl text-[14px] p-5 z-50 w-[350px] h-[50px] border-[1px] border-[#E2DFE3]"
           onKeyDown={(e) => sendQuestion(e)}
           onChange={(event) => setInputValue(event.target.value)}
         />
--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -5,27 +5,29 @@ function ChatWindow(props) {
 
   const isMyMessage = messages.sender === "USER";
   const messageClass = isMyMessage
-    ? "w-[240px] h-89 bg-[#7D69FE] rounded-tl-3xl rounded-tr-3xl rounded-br-none rounded-bl-3xl text-white"
-    : "w-[240px] h-89 bg-[#F3F2FB] rounded-tl-3xl rounded-tr-3xl rounded-bl-none rounded-br-3xl text-gray";
+    ? "w-[240px] h-89 bg-[#7D69FE] rounded-tl-3xl rounded-tr-3xl rounded-br-none rounded-bl-3xl text-white ml-[120px]"
+    : "w-[240px] h-89 bg-[#F3F2FB] rounded-tl-3xl rounded-tr-3xl rounded-bl-none rounded-br-3xl text-gray ml-[54px]";
   const senderClass = isMyMessage
-    ? "text-gray-900 text-right text-[10px] mr-8"
-    : "text-violet-500 text-left text-[10px] ml-8";
-  const positionClass = isMyMessage ? "justify-end mr-5" : "justify-start ml-5";
+    ? "text-gray-900 text-right text-[6px] mr-4"
+    : "text-[#ADA4E2] text-left text-[6px] ml-4 ml-[72px]";
+  const positionClass = isMyMessage ? "justify-end mr-2" : "justify-start ml-2";
 
   return (
     <>
-      <div>
+      <div className="flex flex-row">
         {messages.sender !== "USER" && (
-          <div className="flex flex-row">
-            <div className="w-[36px] h-[36px] bg-[url('/public/logo.jpeg')] bg-cover rounded-lg ml-4" />
-            <div className={`font-light text-sm ${senderClass}`}>
-              STEADIO {messages.sender}
-            </div>
+          <div className="relative">
+            <div className="w-[36px] h-[36px] bg-[url('/public/logo.jpeg')] bg-cover rounded-lg ml-4 absolute bottom-3" />
           </div>
         )}
-        <div className={`flex ${positionClass}`}>
+        <div className={`${positionClass}`}>
+          {messages.sender !== "USER" && (
+            <div className={`font-light ${senderClass}`}>
+              STEADIO {messages.sender}
+            </div>
+          )}
           <li
-            className={`text-sm min-w-10 font-medium min-w-72 mb-3 rounded-lg px-3 py-3 p-5 ${messageClass}`}
+            className={`text-sm min-w-10 font-medium min-w-72 mb-4 rounded-lg px-4 py-3 p-5 ${messageClass}`}
           >
             {messages.text}
           </li>
